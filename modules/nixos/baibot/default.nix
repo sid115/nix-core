b5c633219e2639a288ee2005a95d35d3441595de
sid <sid@portuus.de>
2025-02-09 22:44:56 +0100

    baibot: added nix2yaml parser for config

--- modules/nixos/baibot/default.nix
+++ modules/nixos/baibot/default.nix
@@ -1,9 +1,55 @@
-{ config, lib, ... }:
-
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.services.baibot;
-  defaultConfigPath = "/etc/baibot/config.yml";
-  defaultDataDir = "/var/lib/baibot/data";
+  homeDir = "/var/lib/baibot";
+
+  defaultConfig = {
+    homeserver = {
+      server_name = config.networking.domain;
+      url = "http://127.0.0.1:8008";
+    };
+    user = {
+      mxid_localpart = "baibot";
+      password = "baibot";
+      name = "baibot";
+      encryption = {
+        recovery_passphrase = "long-and-secure-passphrase-here";
+        recovery_reset_allowed = false;
+      };
+    };
+    command_prefix = "!bai";
+    access.admin_patterns = [
+      "@admin:${config.networking.domain}"
+    ];
+    persistence = {
+      data_dir_path = "${homeDir}/data";
+      session_encryption_key = "9701cd109ed56770687dd8410f7d7371a4390dd3feb8ed721f189a0756c40098";
+      config_encryption_key = "a9f1df98d288802ead20a8be2c701a627eabd31cf3d9e2aea28867ccd7a4ded7";
+    };
+    agents.static_definitions = [ ];
+    initial_global_config = {
+      handler = {
+        catch_all = null;
+        text_generation = null;
+        text_to_speech = null;
+        speech_to_text = null;
+        image_generation = null;
+      };
+      user_patterns = [
+        "@*:${config.networking.domain}"
+      ];
+    };
+    logging = "warn,mxlink=debug,baibot=debug";
+  };
+
+  configFile = (pkgs.formats.yaml { }).generate "baibot-config" {
+    inherit (cfg) config;
+  };
 
   inherit (lib)
     mkEnableOption
@@ -16,16 +62,13 @@ in
   options = {
     services.baibot = {
       enable = mkEnableOption "Enable the baibot service, a Matrix AI bot.";
-      configFile = mkOption {
-        type = types.path;
-        default = defaultConfigPath;
-        description = "Path to the baibot configuration file.";
+
+      config = mkOption {
+        type = types.attrs;
+        default = defaultConfig;
+        description = "TODO";
       };
-      dataDir = mkOption {
-        type = types.path;
-        default = defaultDataDir;
-        description = "Path to the baibot data directory.";
-      };
+
       package = mkOption {
         type = types.nullOr types.package;
         default = null;
@@ -49,7 +92,7 @@ in
       users.baibot = {
         isSystemUser = true;
         description = "Baibot system user";
-        home = "/var/lib/baibot";
+        home = homeDir;
         createHome = true;
         group = "baibot";
       };
@@ -57,7 +100,7 @@ in
     };
 
     systemd.tmpfiles.rules = [
-      "d ${cfg.dataDir} 0755 baibot baibot -"
+      "d ${cfg.config.persistence.data_dir_path} 0755 baibot baibot -"
     ];
 
     systemd.services.baibot = {
@@ -65,10 +108,9 @@ in
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       environment = {
-        BAIBOT_CONFIG_FILE_PATH = "${cfg.configFile}";
-        BAIBOT_PERSISTENCE_DATA_DIR_PATH = "${cfg.dataDir}";
+        BAIBOT_CONFIG_FILE_PATH = "${configFile}";
+        BAIBOT_PERSISTENCE_DATA_DIR_PATH = "${cfg.config.persistence.data_dir_path}";
       };
-
       serviceConfig = {
         ExecStart = "${cfg.package}/bin/baibot";
         Restart = "always";
