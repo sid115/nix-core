077f307e65cb56bbc9818b601f00136a87763fe1
sid <sid@portuus.de>
2025-02-21 20:17:28 +0100

    develop: Removed pix2tex

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -14,8 +14,6 @@
   surya-ocr = pkgs.callPackage ./surya-ocr { };
   synapse_change_display_name = pkgs.callPackage ./synapse_change_display_name { };
 
-  pix2tex = pkgs.callPackage ./pix2tex { };
-
   # corryvreckan = pkgs.callPackage ./corryvreckan { }; # FIXME
   # plecs = pkgs.callPackage ./plecs { }; # FIXME
 }
