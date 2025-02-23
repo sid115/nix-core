fd426a93801c7acaab76c4caf7b5338461e503a4
sid <sid@portuus.de>
2025-02-23 14:52:45 +0100

    plecs: Package builds. TODO: Add missing dependencies

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,10 +10,10 @@
   google-genai = pkgs.callPackage ./google-genai { };
   marker-pdf = pkgs.callPackage ./marker-pdf { };
   pdftext = pkgs.callPackage ./pdftext { };
+  plecs = pkgs.callPackage ./plecs { };
   pyman = pkgs.callPackage ./pyman { };
   surya-ocr = pkgs.callPackage ./surya-ocr { };
   synapse_change_display_name = pkgs.callPackage ./synapse_change_display_name { };
 
   # corryvreckan = pkgs.callPackage ./corryvreckan { }; # FIXME
-  # plecs = pkgs.callPackage ./plecs { }; # FIXME
 }
