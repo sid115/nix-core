9ca2cfc9746e2873f80b043ee6c815e99e8078d3
sid <sid@portuus.de>
2025-04-04 01:18:58 +0200

    unstable: first try to run on unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-zoom.url = "github:nixos/nixpkgs/nixos-24.05";
 
     home-manager.url = "github:nix-community/home-manager/release-24.11";
@@ -23,7 +22,6 @@
         "aarch64-linux"
       ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      unstable = inputs.nixpkgs-unstable;
     in
     {
       apps = forAllSystems (
@@ -53,7 +51,7 @@
         }
       );
 
-      packages = forAllSystems (system: import ./pkgs unstable.legacyPackages.${system});
+      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
 
       overlays = import ./overlays { inherit inputs; };
 
