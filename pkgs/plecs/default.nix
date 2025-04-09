4faba8bd68e6a85a1837f71e1e3682b6c01dca9a
sid <sid@portuus.de>
2025-04-09 09:06:25 +0200

    feature/plecs: fixed tls handshake

--- pkgs/plecs/default.nix
+++ pkgs/plecs/default.nix
@@ -11,12 +11,14 @@
   freetype,
   fontconfig,
   zlib,
-  gcc,
+  openssl,
+  cacert,
 }:
 
 stdenv.mkDerivation rec {
   pname = "plecs";
   version = "4.9.4";
+
   src = builtins.fetchTarball {
     url = "https://www.plexim.com/sites/default/files/packages/plecs-standalone-4-9-4_linux64.tar.gz";
     sha256 = "0hkkga1zfnghrivkiws3pqld7ihc3bg1gzx2dckzdd5jfbdd9kpm";
@@ -37,7 +39,9 @@ stdenv.mkDerivation rec {
       freetype
       fontconfig
       zlib
-      gcc.cc.lib
+      stdenv.cc.cc.lib
+      openssl
+      cacert
     ]
     ++ (with qt6; [
       qtbase
@@ -45,31 +49,43 @@ stdenv.mkDerivation rec {
       qt5compat
       qtsvg
       qtwayland
+
       # qtx11extras # not in nixpkgs
       # qtpdf # not in nixpkgs
     ]);
 
   autoPatchelfFlags = [ "--ignore-missing=libQt6Pdf.so.6" ]; # TODO
 
+  SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/plecs $out/bin
     cp -r $src/* $out/plecs/
-    chmod -R +w $out/plecs
 
+    local plecs_lib_path="${lib.makeLibraryPath buildInputs}:$out/plecs/octave/lib/octave/4.4.1:$out/plecs/octave/lib"
+
+    # Wrap executables
     for exec in PLECS PLECS_server crashreporter qhelpgenerator webengine; do
       if [ -f "$out/plecs/$exec" ]; then
-        makeWrapper $out/plecs/$exec $out/bin/$exec \
-          --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}:$out/plecs/octave/lib/octave/4.4.1:$out/plecs/octave/lib" \
+        echo "Wrapping $exec..."
+        makeWrapper "$out/plecs/$exec" "$out/bin/$exec" \
+          --prefix LD_LIBRARY_PATH : "$plecs_lib_path" \
+          --set SSL_CERT_FILE "${SSL_CERT_FILE}" \
           "''${qtWrapperArgs[@]}"
       fi
     done
 
-    # Octave wrapper if needed
+    # Octave wrapper
     if [ -f "$out/plecs/octave/bin/octave" ]; then
-      makeWrapper $out/plecs/octave/bin/octave $out/bin/plecs-octave \
-        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}:$out/plecs/octave/lib/octave/4.4.1:$out/plecs/octave/lib" \
-        "''${qtWrapperArgs[@]}"
+       echo "Wrapping plecs-octave..."
+       makeWrapper "$out/plecs/octave/bin/octave" "$out/bin/plecs-octave" \
+         --prefix LD_LIBRARY_PATH : "$plecs_lib_path" \
+         --set SSL_CERT_FILE "${SSL_CERT_FILE}"
     fi
+
+    runHook postInstall
   '';
 
   meta = {
