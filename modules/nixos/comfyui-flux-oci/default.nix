65f237c87a2ab54bd836c72a36c22fc7862c7920
sid <sid@portuus.de>
2025-04-13 20:13:10 +0200

    develop: comfyui-flux-oci: ExecStartPre has strange behavior in nix...

--- modules/nixos/comfyui-flux-oci/default.nix
+++ modules/nixos/comfyui-flux-oci/default.nix
@@ -74,17 +74,20 @@ in
     };
 
     systemd.services.podman-comfyui-flux = {
-      path = [ pkgs.podman ];
+      path = [
+        pkgs.bash
+        pkgs.podman
+      ];
       serviceConfig = {
         Restart = mkOverride 90 "always";
         ExecStartPre = [
           ''
-            until
-              podman network inspect comfyui-flux-oci_default >/dev/null 2>&1
-            do
-              echo "Waiting for network comfyui-flux-oci_default to be created..."
-              sleep 5
-            done
+            bash -c '
+              until podman network inspect comfyui-flux-oci_default >/dev/null 2>&1; do
+                echo "Waiting for network comfyui-flux-oci_default to be created..."
+                sleep 5
+              done
+            '
           ''
         ];
       };
