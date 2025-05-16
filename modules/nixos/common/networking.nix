7026245a70474a8f94d25149fc63f542ee5428fd
sid <sid@portuus.de>
2025-05-17 01:31:00 +0200

    develop: fixed networking in nixos common module

--- modules/nixos/common/networking.nix
+++ modules/nixos/common/networking.nix
@@ -1,20 +1,25 @@
 { config, lib, ... }:
 
+let
+  inherit (lib) mkDefault;
+
+  isNotEmptyStr = (import ../../../lib).isNotEmptyStr; # FIXME: cannot get lib overlay to work
+in
 {
   config = {
     assertions = [
       {
-        assertion = lib.isNotEmpty config.networking.domain;
+        assertion = isNotEmptyStr config.networking.domain;
         message = "nix-core/nixos/common: config.networking.domain cannot be empty.";
       }
       {
-        assertion = lib.isNotEmpty config.networking.hostName;
+        assertion = isNotEmptyStr config.networking.hostName;
         message = "nix-core/nixos/common: config.networking.hostName cannot be empty.";
       }
     ];
 
     networking = {
-      domain = lib.mkDefault "${config.networking.hostName}.local";
+      domain = mkDefault "${config.networking.hostName}.local";
 
       # NetworkManager
       useDHCP = false;
