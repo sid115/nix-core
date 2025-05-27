32dd0c124559949d568fb4bdfd5a012737e08329
susagi <susagi@sid.ovh>
2025-05-27 18:17:06 +0200

    first attempt of search engine rewrite

--- modules/home/hyprland/applications/librewolf/default.nix
+++ modules/home/hyprland/applications/librewolf/default.nix
@@ -10,6 +10,7 @@ let
   cfg = config.wayland.windowManager.hyprland;
   app = cfg.applications.browser.default;
   desktop = "librewolf.desktop";
+  every_day = 24 * 60 * 60 * 1000; 
   mimeTypes = [
     "text/html"
     "text/xml"
@@ -24,16 +25,50 @@ let
     in
     genMimeAssociations desktop mimeTypes;
 
-  inherit (lib) mkDefault mkIf;
+howlongtobeat = {
+    name = "howlongtobeat";
+    urls = [ { template = "https://howlongtobeat.com/?q={searchTerms}"; } ];
+    icon = "https://howlongtobeat.com/favicon.ico";
+    updateInterval = 1000;
+    definedAliases = [ "@hltb" ];
+  };
+protondb = {
+      name = "protondb";
+      urls = [ { template = "https://www.protondb.com/search?q={searchTerms}"; } ];
+      icon = "https://www.protondb.com/favicon.ico";
+      updateInterval = every_day;
+      definedAliases = [ "@pdb" ];
+    };
+
+
+  inherit (lib) mkDefault mkOption types mkIf;
 in
 {
+options = {
+    programs.librewolf.searchEngines = mkOption {
+      type = types.listOf types.attrs;
+      default = null;
+      description = "A set of search engines for the browser.";
+    };
+  };
+
   config = mkIf (cfg.enable && app == "librewolf") {
     programs.librewolf = {
       enable = true;
+      searchEngines = [
+      protondb
+      howlongtobeat
+      ];
       policies.Homepage.StartPage = mkDefault "previous-session";
       profiles.default = {
         extensions.packages = import ./extensions.nix { inherit inputs pkgs; };
-        search = import ./search.nix { inherit pkgs; };
+        #search = import ./search.nix { inherit pkgs; };
+        search.engines = lib.listToAttrs (
+        map (item: {
+        name = item.name;
+        value = item;
+      }) config.programs.librewolf.searchEngines
+    );
         settings = import ./settings.nix;
         userChrome = builtins.readFile ./userChrome.css;
       };
--- modules/home/hyprland/applications/librewolf/search.nix
+++ modules/home/hyprland/applications/librewolf/search.nix
@@ -1,87 +1,13 @@
-{ pkgs, ... }:
-
+{ config, lib, ... }:
 let
-  every_day = 24 * 60 * 60 * 1000;
-in
-{
-  force = true;
-  default = "Startpage";
-  privateDefault = "Startpage";
-  order = [ "Startpage" ];
-  engines = {
-
-    "GitHub" = {
-      urls = [ { template = "https://github.com/search?q={searchTerms}"; } ];
-      icon = "https://github.com/favicon.ico";
-      updateInterval = every_day;
-      definedAliases = [ "@gh" ];
-    };
-
-    "Home Manager Options" = {
-      urls = [
-        { template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master"; }
-      ];
-      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      definedAliases = [ "@hm" ];
-    };
-
-    "Nix Packages" = {
-      urls = [ { template = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}"; } ];
-      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      definedAliases = [ "@np" ];
-    };
-
-    "NixOS Options" = {
-      urls = [ { template = "https://search.nixos.org/options?channel=unstable&query={searchTerms}"; } ];
-      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      definedAliases = [ "@no" ];
-    };
-
-    "NixOS Wiki" = {
-      urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
-      icon = "https://wiki.nixos.org/favicon.png";
-      updateInterval = every_day;
-      definedAliases = [ "@nw" ];
-    };
-
-    "Nixpkgs Issues" = {
-      urls = [
-        {
-          template = "https://github.com/NixOS/nixpkgs/issues?q=is%3Aissue%20state%3Aopen%20{searchTerms}";
-        }
-      ];
-      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      definedAliases = [ "@ni" ];
-    };
-
-    "NuschtOS Search" = {
-      urls = [ { template = "https://search.xn--nschtos-n2a.de/?query={searchTerms}"; } ];
-      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      definedAliases = [ "@nu" ];
-    };
-
-    "Startpage" = {
-      # TODO: replace with Searx when instance on portuus.de is ready
-      urls = [ { template = "https://www.startpage.com/do/dsearch?q={searchTerms}"; } ];
-      icon = "https://www.startpage.com/sp/cdn/favicons/favicon--default.ico";
-      updateInterval = every_day;
-    };
-
-    "Wikiless" = {
-      urls = [ { template = "https://wikiless.metastem.su/wiki/{searchTerms}"; } ];
-      icon = "https://wikiless.metastem.su/wikiless-favicon.ico";
-      updateInterval = every_day;
-      definedAliases = [ "@wiki" ];
-    };
-
-    youtube = {
-      urls = [ { template = "https://www.youtube.com/results?search_query={searchTerms}"; } ];
-      icon = "https://www.youtube.com/favicon.ico";
-      updateInterval = every_day;
-      definedAliases = [ "@yt" ];
-    };
-
-    protondb = {
+  howlongtobeat = {
+    name = "howlongtobeat";
+    urls = [ { template = "https://howlongtobeat.com/?q={searchTerms}"; } ];
+    icon = "https://howlongtobeat.com/favicon.ico";
+    updateInterval = 1000;
+    definedAliases = [ "@hltb" ];
+  };
+protondb = {
       urls = [ { template = "https://www.protondb.com/search?q={searchTerms}"; } ];
       icon = "https://www.protondb.com/favicon.ico";
       updateInterval = every_day;
@@ -102,16 +28,38 @@ in
       definedAliases = [ "@k4s" ];
     };
 
-    howlongtobeat = {
-      urls = [ { template = "https://howlongtobeat.com/?q={searchTerms}"; } ];
-      icon = "https://howlongtobeat.com/favicon.ico";
-      updateInterval = every_day;
-      definedAliases = [ "@hltb" ];
-    };
+  #  engineList = [
+  #  item1
+  #  howlongtobeat
+  #]; # builtins.listToAttrs [ item1 item3 ];
 
-    # engines below are disabled
-    bing.metaData.hidden = true;
-    ddg.metaData.hidden = true;
-    google.metaData.hidden = true;
+  inherit (lib) types mkOption;
+in
+{
+  options = {
+    list1 = mkOption {
+      type = types.listOf types.attrs;
+      default = null;
+      description = "A list of items.";
+    };
+    programs.librewolf.searchEngines = mkOption {
+      type = types.attrsOf types.any;
+      default = { };
+      description = "A set of search engines for the browser.";
+    };
+  };
+
+  config = {
+    programs.librewolf.searchEngines = [
+      howlongtobeat
+      keyforsteam
+      protondb
+    ];
+    programs.librewolf.profiles.default.search.engines = lib.listToAttrs (
+      map (item: {
+        name = item.name;
+        value = item;
+      }) config.programs.librewolf.searchEngines
+    );
   };
 }
