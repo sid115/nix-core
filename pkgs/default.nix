86036842e80c2b81f104f92420a1c28621e38b92
sid <sid@portuus.de>
2025-06-06 20:06:10 +0200

    develop: removed broken packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,10 +6,6 @@
 {
   baibot = pkgs.callPackage ./baibot { };
   cppman = pkgs.callPackage ./cppman { };
-  marker-pdf = pkgs.callPackage ./marker-pdf { };
   pyman = pkgs.callPackage ./pyman { };
   synapse_change_display_name = pkgs.callPackage ./synapse_change_display_name { };
-
-  # corryvreckan = pkgs.callPackage ./corryvreckan { }; # FIXME
-  # plecs = pkgs.callPackage ./plecs { }; # FIXME
 }
