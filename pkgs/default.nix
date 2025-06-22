e66c95a31f1df8f41d8212d7f2c71409a2bf81e0
sid <sid@portuus.de>
2025-06-22 19:11:51 +0200

    feature/mcpo: implemented nix package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,7 @@
 {
   baibot = pkgs.callPackage ./baibot { };
   cppman = pkgs.callPackage ./cppman { };
+  mcpo = pkgs.callPackage ./mcpo { };
   pyman = pkgs.callPackage ./pyman { };
   synapse_change_display_name = pkgs.callPackage ./synapse_change_display_name { };
 }
