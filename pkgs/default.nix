027ba4e3d6a3de22ec92f1f873716d7536d8c01f
sid <sid@portuus.de>
2025-06-26 22:49:32 +0200

    feature/gemini-cli: added nix package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,7 @@
 {
   baibot = pkgs.callPackage ./baibot { };
   cppman = pkgs.callPackage ./cppman { };
+  gemini-cli = pkgs.callPackage ./gemini-cli { };
   pyman = pkgs.callPackage ./pyman { };
   synapse_change_display_name = pkgs.callPackage ./synapse_change_display_name { };
   visual-paradigm-community = pkgs.callPackage ./visual-paradigm-community { };
