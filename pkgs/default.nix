9917a95cc6407eca8f7e274875b6483cbc831ca9
sid <sid@portuus.de>
2025-06-30 15:27:00 +0200

    develop: gemini-cli is now in nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,7 +6,6 @@
 {
   baibot = pkgs.callPackage ./baibot { };
   cppman = pkgs.callPackage ./cppman { };
-  gemini-cli = pkgs.callPackage ./gemini-cli { };
   mcpo = pkgs.callPackage ./mcpo { };
   pyman = pkgs.callPackage ./pyman { };
   synapse_change_display_name = pkgs.callPackage ./synapse_change_display_name { };
