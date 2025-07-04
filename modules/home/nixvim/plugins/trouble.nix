409b0bbe56f1e42dc28b6b46129be2271a1eb98d
sid <sid@portuus.de>
2025-07-04 16:16:54 +0200

    develop: nixvim trouble keymaps

--- modules/home/nixvim/plugins/trouble.nix
+++ modules/home/nixvim/plugins/trouble.nix
@@ -3,12 +3,19 @@
 let
   cfg = config.programs.nixvim;
   plugin = cfg.plugins.trouble;
+
+  inherit (lib) mkDefault;
 in
 {
   config = {
     programs.nixvim = {
       plugins.trouble = {
-        enable = lib.mkDefault true;
+        enable = mkDefault true;
+        settings = {
+          keys = {
+            "<leader>td" = "diagnostics toggle";
+          };
+        };
       };
     };
   };
