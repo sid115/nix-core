9ac3f7c34c30b816f4c628b6fd04383462cbdb57
sid <sid@portuus.de>
2025-07-04 19:29:18 +0200

    develop: nixvim trouble keymaps

--- modules/home/nixvim/plugins/trouble.nix
+++ modules/home/nixvim/plugins/trouble.nix
@@ -4,20 +4,40 @@ let
   cfg = config.programs.nixvim;
   plugin = cfg.plugins.trouble;
 
-  inherit (lib) mkDefault;
+  inherit (lib) mkDefault mkIf;
 in
 {
   config = {
     programs.nixvim = {
       plugins.trouble = {
         enable = mkDefault true;
-        settings = {
-          keys = {
-            "<leader>xd" = "<cmd>Trouble diagnostics toggle<cr>";
-            "<leader>xq" = "<cmd>Trouble qflist toggle<cr>";
-          };
-        };
       };
+      keymaps = mkIf plugin.enable [
+        {
+          mode = "n";
+          key = "<leader>xq";
+          action = "<CMD>Trouble qflist toggle<CR>";
+          options = {
+            desc = "Trouble quifick toggle";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>xl";
+          action = "<CMD>Trouble loclist toggle<CR>";
+          options = {
+            desc = "Trouble loclist toggle";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>xx";
+          action = "<CMD>Trouble diagnostics toggle<CR>";
+          options = {
+            desc = "Trouble diagnostics toggle";
+          };
+        }
+      ];
     };
   };
 }
