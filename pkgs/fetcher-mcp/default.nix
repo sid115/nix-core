012bf40d7b5f5977cfda3576c81b246d9f740a8b
sid <sid@portuus.de>
2025-07-24 15:19:04 +0200

    develop: added packages fetcher-mcp and trelis-gitingest-mcp

---
+++ pkgs/fetcher-mcp/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildNpmPackage,
+  fetchFromGitHub,
+  makeWrapper,
+  playwright,
+}:
+
+buildNpmPackage rec {
+  pname = "fetcher-mcp";
+  version = "unstable-2025-06-26";
+
+  src = fetchFromGitHub {
+    owner = "jae-jae";
+    repo = "fetcher-mcp";
+    rev = "652ec7b4f79020fea4924d1edb916d1dbf8755fe";
+    hash = "sha256-9tkZnY0d10ECoqp6gs1qTYbZFS6WqFtjwTAR+tHajng=";
+  };
+
+  npmDepsHash = "sha256-1Pw+W3OdtGmgu1n4nacwaX77nfRUdWWkorn/xuiBhkA=";
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  makeWrapperArgs = [
+    "--set-default"
+    "PLAYWRIGHT_BROWSERS_PATH"
+    "${playwright.browsers}"
+  ];
+
+  meta = {
+    description = "MCP server for fetch web page content using Playwright headless browser";
+    homepage = "https://github.com/jae-jae/fetcher-mcp";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "fetcher-mcp";
+    platforms = lib.platforms.all;
+  };
+}
---
+++ pkgs/trelis-gitingest-mcp/default.nix
@@ -0,0 +1,40 @@
+# See https://github.com/coderamp-labs/gitingest/issues/245
+{
+  lib,
+  python3,
+  fetchPypi,
+}:
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "trelis-gitingest-mcp";
+  version = "1.1.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "trelis_gitingest_mcp";
+    inherit version;
+    hash = "sha256-ZOoG0nL0+XnyOUPo4qsGTYizAPzSECUr9eSHEp4Hmzc=";
+  };
+
+  build-system = [
+    python3.pkgs.hatchling
+  ];
+
+  dependencies = with python3.pkgs; [
+    gitingest
+    mcp
+    pathspec
+  ];
+
+  pythonImportsCheck = [
+    "gitingest_mcp"
+  ];
+
+  meta = {
+    description = "An MCP server for gitingest";
+    homepage = "https://pypi.org/project/trelis-gitingest-mcp/";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "trelis-gitingest-mcp";
+  };
+}
