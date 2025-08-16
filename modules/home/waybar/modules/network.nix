9e26d5d32f1b5d9326feeb1f8efb18d93b295d4e
sid <sid@portuus.de>
2025-08-16 11:41:07 +0200

    develop: edited waybar network widget tooltip formats

--- modules/home/waybar/modules/network.nix
+++ modules/home/waybar/modules/network.nix
@@ -25,15 +25,26 @@ in
     Icon: {icon}
   '';
   tooltip-format-wifi = mkDefault ''
+    Interface: {ifname}
+    IP Address: {ipaddr}/{cidr}
+    Gateway: {gwaddr}
+    Netmask: {netmask}
     ESSID: {essid}
     Signal Strength: {signalStrength}%
     Signal dBm: {signaldBm} dBm
     Frequency: {frequency} GHz
+    Bandwidth Up: {bandwidthUpBits} bps / {bandwidthUpBytes} Bps
+    Bandwidth Down: {bandwidthDownBits} bps / {bandwidthDownBytes} Bps
+    Total Bandwidth: {bandwidthTotalBits} bps / {bandwidthTotalBytes} Bps
   '';
   tooltip-format-ethernet = mkDefault ''
     Interface: {ifname}
     IP Address: {ipaddr}/{cidr}
     Gateway: {gwaddr}
+    Netmask: {netmask}
+    Bandwidth Up: {bandwidthUpBits} bps / {bandwidthUpBytes} Bps
+    Bandwidth Down: {bandwidthDownBits} bps / {bandwidthDownBytes} Bps
+    Total Bandwidth: {bandwidthTotalBits} bps / {bandwidthTotalBytes} Bps
   '';
   tooltip-format-disconnected = mkDefault "Disconnected";
 }
