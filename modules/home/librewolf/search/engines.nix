857c94e2198c93bd79998ab998bb83c7cf9d423a
sid <sid@portuus.de>
2025-08-18 09:15:58 +0200

    develop: search engines are now referenced by id. added noogle.

--- modules/home/librewolf/search/engines.nix
+++ modules/home/librewolf/search/engines.nix
@@ -1,79 +1,85 @@
 pkgs:
 
 {
-  "GitHub" = {
+  github = {
     url = "https://github.com/search?q={searchTerms}";
     icon = "https://github.com/favicon.ico";
     alias = "@gh";
   };
 
-  "Home Manager Options" = {
+  home-manager-options = {
     url = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master";
     icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
     alias = "@hm";
   };
 
-  "Nix Packages" = {
+  nixpkgs = {
     url = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}";
     icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
     alias = "@np";
   };
 
-  "NixOS Options" = {
+  nixos-options = {
     url = "https://search.nixos.org/options?channel=unstable&query={searchTerms}";
     icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
     alias = "@no";
   };
 
-  "NixOS Wiki" = {
+  nixos-wiki = {
     url = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
     icon = "https://wiki.nixos.org/favicon.png";
     alias = "@nw";
   };
 
-  "Nixpkgs Issues" = {
-    url = "https://github.com/NixOS/nixpkgs/issues?q=is%3Aissue%20state%3Aopen%20{searchTerms}";
+  noogle = {
+    url = "https://noogle.dev/q?term={searchTerms}";
     icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-    alias = "@ni";
+    alias = "@nf";
   };
 
-  "NuschtOS Search" = {
+  nuschtos = {
     url = "https://search.xn--nschtos-n2a.de/?query={searchTerms}";
     icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
     alias = "@nu";
   };
 
-  "Wikiless" = {
+  nixpkgs-issues = {
+    url = "https://github.com/NixOS/nixpkgs/issues?q=is%3Aissue%20state%3Aopen%20{searchTerms}";
+    icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+    alias = "@ni";
+  };
+
+  wikiless = {
     url = "https://wikiless.metastem.su/wiki/{searchTerms}";
     icon = "https://wikiless.metastem.su/wikiless-favicon.ico";
     alias = "@wiki";
   };
 
-  "YouTube" = {
+  youtube = {
     url = "https://www.youtube.com/results?search_query={searchTerms}";
     icon = "https://www.youtube.com/favicon.ico";
     alias = "@yt";
   };
 
-  "ProtonDB" = {
+  protondb = {
     url = "https://www.protondb.com/search?q={searchTerms}";
     icon = "https://www.protondb.com/favicon.ico";
     alias = "@pdb";
   };
 
-  "SteamDB" = {
+  steamdb = {
     url = "https://steamdb.info/search/?a=all&q={searchTerms}";
     icon = "https://steamdb.info/favicon.ico";
     alias = "@stdb";
   };
 
-  "keyforsteam" = {
+  keyforsteam = {
     url = "https://www.keyforsteam.de/katalog/?search_name={searchTerms}";
     icon = "https://www.keyforsteam.de/favicon.ico";
     alias = "@k4s";
   };
 
-  "HowLongToBeat" = {
+  howlongtobeat = {
     url = "https://howlongtobeat.com/?q={searchTerms}";
     icon = "https://howlongtobeat.com/favicon.ico";
     alias = "@hltb";
