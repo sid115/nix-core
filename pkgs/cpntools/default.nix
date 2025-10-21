919637f92bee8b9deac0c13226266d8381af5127
sid <sid@portuus.de>
2025-10-21 14:39:01 +0200

    feature/cpntools: added 32bit libs

--- pkgs/cpntools/default.nix
+++ pkgs/cpntools/default.nix
@@ -1,7 +1,24 @@
 {
-  stdenv,
   lib,
-  makeWrapper,
+  stdenv,
+  wrapGAppsHook,
+  gdk-pixbuf,
+  glib,
+  gtk2,
+  libX11,
+  libXcursor,
+  libXext,
+  libXft,
+  libXi,
+  libXinerama,
+  libXrandr,
+  libXrender,
+  pango,
+  zlib,
+  fontconfig,
+  freetype,
+  openssl,
+  pkgsi686Linux,
 }:
 
 stdenv.mkDerivation rec {
@@ -10,20 +27,110 @@ stdenv.mkDerivation rec {
 
   src = builtins.fetchTarball {
     url = "https://cpntools.org/downloads/cpntools_${version}.tar.gz";
-    sha256 = "";
+    sha256 = "sha256:1h5lqf6knwfmm0g15vh1a16aakk6fin3i4wzxbmby30nnl3sxrcd";
   };
 
   nativeBuildInputs = [
-    makeWrapper
+    wrapGAppsHook
   ];
 
-  dontBuild = true;
-  dontCheck = true;
+  i686Libs = with pkgsi686Linux; [
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    glibc
+    gtk2
+    libX11
+    libXcursor
+    libXext
+    libXft
+    libXi
+    libXinerama
+    libXrandr
+    libXrender
+    libxml2
+    openssl
+    pango
+    zlib
+  ];
+
+  buildInputs = [
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gtk2
+    libX11
+    libXcursor
+    libXext
+    libXft
+    libXi
+    libXinerama
+    libXrandr
+    libXrender
+    openssl
+    pango
+    zlib
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin $out/lib/cpntools
+    cp -r ./* $out/lib/cpntools/
+
+    # Create wrapper script
+    mkdir -p $out/bin
+    cat > $out/bin/cpntools <<EOF
+    #!/bin/sh
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$out/lib/cpntools"
+    exec $out/lib/cpntools/cpntools "\$@"
+    EOF
+    chmod +x $out/bin/cpntools
+
+    # Install fonts
+    mkdir -p $out/share/fonts/truetype/cpntools
+    cp fonts/*.ttf $out/share/fonts/truetype/cpntools/
+    cp fonts/*.pfa $out/share/fonts/truetype/cpntools/
+
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    mkdir -p $out/etc/fonts
+    ln -s ${fontconfig.out}/etc/fonts/fonts.conf $out/etc/fonts/fonts.conf
+
+    cpntools_bin="$out/lib/cpntools/cpntools"
+    if [ -f "$cpntools_bin" ]; then
+      patchelf \
+        --set-interpreter "${pkgsi686Linux.glibc}/lib/ld-linux.so.2" \
+        --set-rpath "${lib.makeLibraryPath i686Libs}" \
+        "$cpntools_bin"
+    fi
+
+    cpnmld_bin="$out/lib/cpntools/cpnsim/cpnmld"
+    if [ -f "$cpnmld_bin" ]; then
+      patchelf \
+        --set-interpreter "${pkgsi686Linux.glibc}/lib/ld-linux.so.2" \
+        --set-rpath "${lib.makeLibraryPath i686Libs}" \
+        "$cpnmld_bin"
+    fi
+
+    run_bin="$out/lib/cpntools/cpnsim/run.x86-linux"
+    if [ -f "$run_bin" ] && file "$run_bin" | grep -q 'ELF.*executable'; then
+      patchelf \
+        --set-interpreter "${pkgsi686Linux.glibc}/lib/ld-linux.so.2" \
+        --set-rpath "${lib.makeLibraryPath i686Libs}" \
+        "$run_bin"
+    fi
+  '';
 
   meta = with lib; {
-    description = "A tool for editing, simulating, and analyzing Colored Petri nets";
-    homepage = "https://cpntools.org/";
-    # license = licenses.unfree;
-    platforms = platforms.linux;
+    description = "Computer-Aided Analysis of Coloured Petri Nets";
+    homepage = "http://cpntools.org/";
+    license = licenses.unfree;
+    maintainers = with maintainers; [ ];
+    platforms = [ "x86_64-linux" ];
   };
 }
