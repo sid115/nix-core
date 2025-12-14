4601276d87634b8d0f034d78693044679aec41dd
sid <sid@portuus.de>
2025-12-14 17:26:08 +0100

    feature/smtp-relay: update module options to unstable

--- modules/nixos/smtp-relay/default.nix
+++ modules/nixos/smtp-relay/default.nix
@@ -1,12 +1,9 @@
 { config, lib, ... }:
 
-# Set in your SNM config:
-# relayHost = "[SMTP_RELAY_SERVER_IP]"; # `[]` tell Postfix to use IP address (no MX lookup)
-# relayPort = 25;
-# relaySsl = null; # let Tailscale handle encryption
-
 let
   cfg = config.services.smtp-relay;
+  domain = config.networking.domain;
+  fqdn = "${cfg.subdomain}.${domain}";
 
   inherit (lib)
     mkEnableOption
@@ -28,25 +25,48 @@ in
       description = "IP address of the mail server";
       example = "100.64.0.1";
     };
-    interface = mkOption {
-      type = types.str;
-      description = "Network interface to bind the SMTP relay server";
-      example = "tailscale0";
-    };
   };
 
   config = mkIf cfg.enable {
-    networking.firewall.interfaces."${cfg.interface}".allowedTCPPorts = [ 25 ];
+    networking.firewall.allowedTCPPorts = [
+      25
+      465
+      587
+    ];
+
+    security.acme.certs."${fqdn}" = {
+      group = "postfix";
+    };
 
     services.postfix = {
       enable = true;
-      hostname = "${cfg.subdomain}.${config.networking.domain}";
-      config = {
-        inet_interfaces = "all";
-        inet_protocols = "ipv4";
-        mynetworks = "127.0.0.0/8 [::1]/128 ${cfg.mailserverIP}/32";
-        smtp_tls_security_level = "may";
-        smtpd_recipient_restrictions = "permit_mynetworks, reject_unauth_destination";
+
+      transport = ''
+        ${domain} smtp:${cfg.mailserverIP}
+      '';
+
+      settings.main = {
+        myhostname = fqdn;
+        mydomain = domain;
+        mydestination = [ "localhost" ];
+        relay_domains = [ domain ];
+        mynetworks = [
+          "127.0.0.0/8"
+          "[::1]/128"
+          "${cfg.mailserverIP}/32"
+        ];
+        transport_maps = [ "hash:/etc/postfix/transport" ];
+        smtpd_tls_chain_files = with config.security.acme.certs."${fqdn}"; [
+          "${directory}/key.pem"
+          "${directory}/fullchain.pem"
+        ];
+        smtpd_tls_security_level = "may";
+        smtpd_recipient_restrictions = [
+          "permit_mynetworks"
+          "reject_unauth_destination"
+          "reject_unverified_recipient"
+        ];
+        unverified_recipient_reject_code = 550;
       };
     };
   };
