8ebfd7ab7402cf5d384391be27e57304a7ce30dc
sid <sid@portuus.de>
2026-01-01 14:22:33 +0100

    hotfix: remove netcat-openbsd because of build failure
    (cherry picked from commit 05ed3c35a70ed1785246ff616dc9b3f390cbae20)

--- modules/nixos/common/packages.nix
+++ modules/nixos/common/packages.nix
@@ -12,7 +12,7 @@
     lm_sensors
     lsof
     neovim
-    netcat-openbsd
+    # netcat-openbsd # HOTFIX: does not build: https://hydra.nixos.org/build/317878771
     nettools
     nix-init
     nixfmt-rfc-style
